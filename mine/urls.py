66c316a86fa156940cf08fb9d7617af6a3163e4f
godfreykarugu <godfreykarugu77@gmail.com>
2024-04-22 21:29:35 -0700

    included admin, model & contact form

--- mine/urls.py
+++ mine/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
    path('about/',views.about, name='about'),
    path('services/',views.services, name='services'),
    path('project/',views.project, name='project'),
-   path('contact/',views.contact, name='contct'),
+   path('contact/',views.contact, name='contact'),
 ]
